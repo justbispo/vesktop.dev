f963f4da402c62ea129674cf6798172e6b29a812
Vendicated <vendicated@riseup.net>
2025-06-27 01:19:34 +0200

    initialise starlight project

---
+++ astro.config.mjs
@@ -0,0 +1,46 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [
+        starlight({
+            title: "Vesktop",
+            description: "Vesktop is a custom desktop client for Discord",
+            logo: {
+                src: "./src/assets/vesktop.svg"
+            },
+
+            social: [
+                { icon: "github", label: "GitHub", href: "https://github.com/Vencord/Vesktop" },
+                { icon: "discord", label: "Discord", href: "https://vencord.dev/discord" }
+            ],
+
+            editLink: {
+                baseUrl: "https://github.com/Vencord/vesktop.vencord.dev/tree/main/"
+            },
+
+            lastUpdated: true,
+            pagination: false,
+            tableOfContents: false,
+
+            sidebar: [
+                {
+                    label: "Installing",
+                    items: [
+                        { label: "Windows", slug: "install/windows" },
+                        { label: "macOS", slug: "install/mac" },
+                        { label: "Linux", slug: "install/linux" }
+                    ]
+                },
+                {
+                    label: "Wiki",
+                    autogenerate: {
+                        directory: "wiki"
+                    }
+                }
+            ]
+        })
+    ]
+});
